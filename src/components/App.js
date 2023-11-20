818391b3d053101c1ef203dc699a4772e988a6ff
Allan Kinyua Murigi <allan.murigi@student.moringaschool.com>
2023-11-20 20:59:34 +0300

    cotinuation

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ const App = () => {
 
   useEffect(() => {
     
-    fetch('https://my-json-server.typicode.com/BROGRAMMER025/BotBattlerCodeChallenge/db')
+    fetch('')
       .then((response) => response.json())
       .then((data) => setBots(data.bots))
       .catch((error) => console.error('Error fetching bots:', error));
